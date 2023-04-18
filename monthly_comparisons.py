794af0d8515f7319211729bca927588f454fd33d
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-18 18:07:44 +0100

    Created bar graph

--- monthly_comparisons.py
+++ monthly_comparisons.py
@@ -9,7 +9,7 @@ print("Total Sales by Month in descending order")
 print()
 monthly_sales = df.pivot_table(index=['Month'], values=['Total Sales'], aggfunc='sum')
 sort_monthly_sales = monthly_sales.sort_values(by=['Total Sales'], ascending=False)
-
+print(sort_monthly_sales)
 
 sort_monthly_sales.to_csv('month_comparisons.csv')
 
@@ -19,4 +19,12 @@ totalsales = df2["Total Sales"]
 plt.pie(totalsales, labels=months,
         autopct='%1.f%%',)
 plt.title("Percentage of Sales each Month")
+plt.show()
+
+
+plt.bar(df2["Month"], df2["Total Sales"])
+plt.title("Total Sales per Month", fontsize=16)
+plt.xlabel("Month", fontsize=14)
+plt.ylabel("Total Sales", fontsize=14)
+plt.grid(True)
 plt.show()
