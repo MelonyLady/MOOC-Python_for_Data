f4f0597c9e3362182c4e5a683155d1bd6ef45e5f
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-18 17:15:50 +0100

    aggfunc='sum' all that was missing

--- monthly_comparisons.py
+++ monthly_comparisons.py
@@ -1,27 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 df = pd.read_csv('sales_dataset.csv')
 
-
 df['Total Sales'] = df['Quantity Sold'] * df['Sale Price']
-monthly_sales = df.pivot_table(index=['Month', 'Product Name'], values=['Quantity Sold', 'Sale Price', 'Total Sales'])
-print(monthly_sales)
 
-plt.bar(df['Month'], df['Total Sales'])
-plt.title('Total Sales per Month', fontsize=14)
-plt.xlabel('Month', fontsize=14)
-plt.xticks(rotation=30, horizontalalignment="center")
-plt.ylabel('Total Sales', fontsize=14)
-plt.grid(True)
-plt.show()
 
-df.groupby(['Month']).sum().plot(kind='pie', y='Total Sales', autopct='%1.0f%%',
-                                 title='Percentage of Sales per Month')
-plt.show()
+# plt.bar(df['Month'], df['Total Sales'])
+# plt.title('Total Sales per Month', fontsize=14)
+# plt.xlabel('Month', fontsize=14)
+# plt.xticks(rotation=30, horizontalalignment="center")
+# plt.ylabel('Total Sales', fontsize=14)
+# plt.grid(True)
+# plt.show()
+#
+# df.groupby(['Month']).sum().plot(kind='pie', y='Total Sales', autopct='%1.0f%%',
+#                                  title='Percentage of Sales per Month')
+# plt.show()
 
 
 print("Total Sales by Month in descending order")
 print()
-print(df.groupby('Month')['Total Sales'].max().reset_index().sort_values(['Total Sales'], ascending=False))
+
+
+monthly_sales = df.pivot_table(index=['Month'], values=['Total Sales'], aggfunc='sum')
+print(monthly_sales.sort_values(by=['Total Sales'], ascending=False))
