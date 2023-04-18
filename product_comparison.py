29c2c482c3b12aa121bf4544426a04437e7c9ba4
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-18 19:16:44 +0100

    printed information to csv files

--- product_comparison.py
+++ product_comparison.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('csv_files/sales_dataset.csv')
 
 
+# creation of Total Sales column
 df['Total Sales'] = df['Quantity Sold'] * df['Sale Price']
 
 
-
+# bar chart for Total Product Sales
 plt.bar(df['Product Name'], df['Total Sales'])
 plt.title('Total Product Sales', fontsize=14)
 plt.xlabel('Product Name', fontsize=14)
@@ -17,14 +18,21 @@ plt.ylabel('Total Sales', fontsize=14)
 plt.grid(True)
 plt.show()
 
+product_sales = df.groupby('Product Name')['Total Sales'].max().reset_index().sort_values(['Total Sales'], ascending=False)
+product_sales.to_csv('csv_files/product_sales.csv')
+
 print()
 print()
 print("Total Sales by Product in descending order")
 print()
-print(df.groupby('Product Name')['Total Sales'].max().reset_index().sort_values(['Total Sales'], ascending=False))
+print(product_sales)
+
+
+avg_sale_price = df.groupby('Category')['Sale Price'].mean()
+avg_sale_price.to_csv('csv_files/avg_sale_prices.csv')
 
 print()
 print()
 print('Average Sale Price per Category')
 print()
-print(df.groupby('Category')['Sale Price'].mean())
+print(avg_sale_price)
