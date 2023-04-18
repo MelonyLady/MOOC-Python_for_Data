4a1be57d59bd80128989feca1cda6197b40fe157
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-18 18:02:19 +0100

    created csv file for month comparisons, and created pie chart

--- monthly_comparisons.py
+++ monthly_comparisons.py
@@ -5,25 +5,18 @@ df = pd.read_csv('sales_dataset.csv')
 
 df['Total Sales'] = df['Quantity Sold'] * df['Sale Price']
 
-
-# plt.bar(df['Month'], df['Total Sales'])
-# plt.title('Total Sales per Month', fontsize=14)
-# plt.xlabel('Month', fontsize=14)
-# plt.xticks(rotation=30, horizontalalignment="center")
-# plt.ylabel('Total Sales', fontsize=14)
-# plt.grid(True)
-# plt.show()
-#
-# df.groupby(['Month']).sum().plot(kind='pie', y='Total Sales', autopct='%1.0f%%',
-#                                  title='Percentage of Sales per Month')
-# plt.show()
-
-
 print("Total Sales by Month in descending order")
 print()
-
-
 monthly_sales = df.pivot_table(index=['Month'], values=['Total Sales'], aggfunc='sum')
-print(monthly_sales.sort_values(by=['Total Sales'], ascending=False))
+sort_monthly_sales = monthly_sales.sort_values(by=['Total Sales'], ascending=False)
 
 
+sort_monthly_sales.to_csv('month_comparisons.csv')
+
+df2 = pd.read_csv('month_comparisons.csv')
+months = df2["Month"]
+totalsales = df2["Total Sales"]
+plt.pie(totalsales, labels=months,
+        autopct='%1.f%%',)
+plt.title("Percentage of Sales each Month")
+plt.show()
